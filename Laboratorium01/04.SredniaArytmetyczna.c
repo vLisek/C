8ceb2ac4076843317298d7705939347a34cfb344
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2024-11-10 18:06:24 +0100

    Create 04.SredniaArytmetyczna.c

---
+++ Laboratorium01/04.SredniaArytmetyczna.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Zadanie 4.\n");
+    float Num4_1, Num4_2, Num4_3, Num4_4, Avg;
+    printf("Podaj cztery liczby oddzielone spacjami: ");
+    scanf_s("%f %f %f %f", &Num4_1, &Num4_2, &Num4_3, &Num4_4);
+    
+    Avg = (Num4_1 + Num4_2 + Num4_3 + Num4_4) / 4;
+    printf("\nŚrednia arytmetyczna: %.2f\n", Avg);
+}

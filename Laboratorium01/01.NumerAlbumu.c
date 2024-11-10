b7addb7793428adbb7758a070ed4a4aaa482c030
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2024-11-10 17:27:06 +0100

    Create 01.NumerAlbumu.c

---
+++ Laboratorium01/01.NumerAlbumu.c
@@ -0,0 +1,14 @@
+// -------------------- Zadanie 1 --------------------
+// |                                                 |
+// |  Napisać program, który wypisuje na konsolę nr  |
+// |  albumu studenta (osoby rozwiązującej program). |
+// |                                                 |
+// |              Autor: Filip Lisowski              |
+// ---------------------------------------------------
+
+#include <stdio.h>
+
+int main() {
+    printf("Numer albumu: 134942\n");
+    return 0;
+}

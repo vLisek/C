fa893286f062953e4823ba9bb67da11184ae2606
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2024-11-10 17:31:24 +0100

    Create PorownanieLiczb.c

---
+++ Laboratorium01/03.PorownanieLiczb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float Num3_1, Num3_2;
+    printf("Zadanie 3.\n");
+    printf("Podaj pierwszą liczbę: ");
+    scanf_s("%f", &Num3_1);
+    printf("Podaj drugą liczbę: ");
+    scanf_s("%f", &Num3_2);
+    
+    if (Num3_1 > Num3_2) {
+        printf("\nPierwsza liczba (%.2f) jest większa.\n", Num3_1);
+    }
+    else if (Num3_1 < Num3_2) {
+        printf("\nDruga liczba (%.2f) jest większa.\n", Num3_2);
+    }
+    else {
+        printf("\nObie liczby są równe.\n");
+    }
+    
+    system("pause");
+    printf("\n\n");
+
+    return 0;
+}

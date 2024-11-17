c59c30d82bcc60ef46287b993a1e36f1d1033f15
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2024-11-17 14:30:46 +0100

    Update 03. Suma parzystych.c

--- Laboratorium02/03. Suma parzystych.c
+++ Laboratorium02/03. Suma parzystych.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+int main() {
+    SetConsoleOutputCP(1250);
+
+    // -------------------- Zadanie 3 --------------------
+    // |                                                 |
+    // |  Dany jest ciąg n liczb (n > 0), napisz program |
+    // |  pozwalający na obliczenie sumy wszystkich      |
+    // |  liczb parzystychw danym ciągu.                 |
+    // |                                                 |
+    // |              Autor: Filip Lisowski              |
+    // ---------------------------------------------------
+
+    printf("Zadanie 3.\n");
+
+    int n;
+    int Sum1 = 0;
+
+    printf("Podaj liczbę n: ");
+    scanf_s("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            Sum1 += i;
+        }
+    }
+
+    printf("Suma liczb parzystych w ciągu od 1 do %d: %d\n\n", n, Sum1);
+    return 0;
+}

555157a51bc17a687eed4ee366d888de68044e49
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2024-11-17 13:27:13 +0100

    Update 04. Srednia Arytmetyczna.c

--- Laboratorium01/04. Srednia Arytmetyczna.c
+++ Laboratorium01/04. Srednia Arytmetyczna.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
+    SetConsoleOutputCP(1250);
+    
     printf("Zadanie 4.\n");
     float Num4_1, Num4_2, Num4_3, Num4_4, Avg;
     printf("Podaj cztery liczby oddzielone spacjami: ");

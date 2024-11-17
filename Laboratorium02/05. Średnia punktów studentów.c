0859bf35f8730a54441c71c45be6b05ac274f964
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2024-11-17 14:07:39 +0100

    Update 05. Średnia punktów studentów.c

--- Laboratorium02/05. Średnia punktów studentów.c
+++ Laboratorium02/05. Średnia punktów studentów.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+int main() {
+    SetConsoleOutputCP(1250);
+    // -------------------- Zadanie 5 --------------------
+    // |                                                 |
+    // |  Grupa laboratoryjna składa się z n studentów   |
+    // |  (wartość n podaje użytkownik). Wprowadzamy     |
+    // |  liczbę punktów dla każdego studenta. Napisz    |
+    // |  program, który obliczy średnią liczbę punktów  |
+    // |  w grupie z wykorzystaniem pętli while.         |
+    // |                                                 |
+    // |              Autor: Filip Lisowski              |
+    // ---------------------------------------------------
+
+
+    int studenci;
+    int suma = 0;
+    int punkt;
+    int i = 0;
+    float srednia;
+
+    printf("Podaj liczbe studentow: ");
+    scanf_s("%d", &studenci);
+
+    while (i < studenci) {
+        printf("Podaj punkty studenta %d: ", i + 1);
+        scanf_s("%d", &punkt);
+        suma += punkt;
+        i++;
+    }
+
+    srednia = (float)suma / studenci;
+
+    printf("Srednia liczba punktow: %.2f\n", srednia);
+ 
+    return 0;
+}

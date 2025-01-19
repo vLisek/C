4b3bbb2852deec764b6df4065f375d612b32533e
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2025-01-19 17:56:29 +0100

    Update Zadanie 8.c

--- Laboratorium06/Zadanie 8.c
+++ Laboratorium06/Zadanie 8.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+void swap_values(int* VarA, int* VarB) {
+    int Temp = *VarA;
+    *VarA = *VarB;
+    *VarB = Temp;
+}
+
+int main() {
+    SetConsoleOutputCP(1250);
+
+    printf("Zadanie 8.\n\n");
+
+    int VarX; 
+    int VarY;
+
+    printf("Podaj pierwsza liczbe: ");
+    scanf_s("%d", &VarX);
+    printf("Podaj druga liczbe: ");
+    scanf_s("%d", &VarY);
+
+    printf("Przed zamianą: x = %d, y = %d\n", VarX, VarY);
+    
+    swap_values(&VarX, &VarY);
+
+    printf("Po zamianie: x = %d, y = %d\n", VarX, VarY);
+
+    return 0;
+}

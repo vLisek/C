5240b802210bc6158a9ecd14fc77ea426f32e32f
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2025-01-19 17:47:33 +0100

    Update Zadanie 7.c

--- Laboratorium06/Zadanie 7.c
+++ Laboratorium06/Zadanie 7.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+
+int MinValueAndPointer(int* VarA, int* VarB, int** MinPtr) {
+    if (*VarA < *VarB) {
+        *MinPtr = VarA;
+        return *VarA;
+    }
+    else {
+        *MinPtr = VarB;
+        return *VarB;
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1250);
+
+    printf("Zadanie 7.\n\n");
+
+    int VarX;
+    int VarY;
+
+    printf("Podaj pierwszą liczbę: ");
+    scanf_s("%d", &VarX);
+    printf("Podaj drugą liczbę: ");
+    scanf_s("%d", &VarY);
+
+    int* MinPtr;
+
+    int MinValue = MinValueAndPointer(&VarX, &VarY, &MinPtr);
+
+    printf("Mniejsza wartość: %d\n", MinValue);
+    printf("Adres mniejszej wartości: %p\n", &MinPtr);
+
+    return 0;
+}
 

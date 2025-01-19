47f1f5cd358d3131e27e4f87a7f62c1f21094598
Filip Lisowski <106061451+vLisek@users.noreply.github.com>
2025-01-19 18:23:58 +0100

    Update Zadanie 1.c

--- Laboratorium07/Zadanie 1.c
+++ Laboratorium07/Zadanie 1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+    typedef struct {
+        int VarA;
+        int VarB;
+        int VarC;
+    } Triangle;
+    
+    int obwod_trojkata(Triangle t) {
+        return t.VarA + t.VarB + t.VarC;
+    }
+
+    int main() {
+        SetConsoleOutputCP(1250);
+
+        printf("Zadanie 1.\n");
+
+        Triangle t = { 1, 3, 4 };
+        printf("Obwód trójkąta: %d\n", obwod_trojkata(t));
+
+        return 0;
+    }
